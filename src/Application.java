6da2d361cad5cc6fa01c0db78f744fd70777e9b0
abi-seth <abiheloaf@gmail.com>
2022-02-09 11:08:45 +0200

    New: advanced extraction enabled

--- src/Application.java
+++ src/Application.java
@@ -1,4 +1,5 @@
 import customs.OutputMessageTypes;
+import customs.PromoteUserInput;
 import customs.ShowOptions;
 import functions.GetWebsiteNavigationLinks;
 import utils.MessageHandler;
@@ -10,28 +11,17 @@ public class Application {
     private static ShowOptions ChooseOptions = new ShowOptions();
     private static GetWebsiteNavigationLinks getUrl = new GetWebsiteNavigationLinks();
 
-    public static int RepeatProcess(Boolean status) {
-        if (status) {
-            option = ChooseOptions.generateOptions();
-        } else {
-            return 0;
-        }
-        return 0;
-    }
-
-    private static int exit() {
-        MessageHandler.printConsoleMessage(OutputMessageTypes.SUCCESS, "Bye!");
-        return 0;
-    }
-
-    public static void main(String [] args) throws IOException {
-        System.out.println("\n============= WELCOME TO WEBSITE CODE CLONER =============\n");
+    public static int doYouWantContinue() {
         try {
-            RepeatProcess(true);
+            System.out.println("Do you want to extract another website?:");
+            System.out.println("\t1. Yes");
+            System.out.println("\t0. No");
+            PromoteUserInput promoteInput = new PromoteUserInput();
+            int promotevalue = promoteInput.promoteUserInt("Enter option: ");
 
-            switch (option) {
+            switch (promotevalue) {
                 case 1:
-                    getUrl.getWebsiteUrl();
+                    RepeatProcess(true);
                     break;
                 case 0:
                     exit();
@@ -42,6 +32,58 @@ public class Application {
         } catch (Exception error) {
             MessageHandler.printConsoleMessage(OutputMessageTypes.ERROR, error.getMessage());
         }
+        return 0;
+    }
 
+    private static int exit() {
+        MessageHandler.printConsoleMessage(OutputMessageTypes.SUCCESS, "Bye!");
+        return 0;
+    }
+
+    public static int RepeatProcess(Boolean status) {
+        try {
+            if (status) {
+                option = ChooseOptions.generateOptions();
+
+                switch (option) {
+                    case 1:
+                        getUrl.getWebsiteUrl();
+                        doYouWantContinue();
+                        break;
+                    case 0:
+                        exit();
+                        break;
+                    default:
+                        MessageHandler.printConsoleMessage(OutputMessageTypes.ERROR, "Incorrect choice!");
+                }
+            } else {
+                return 0;
+            }
+        } catch(Exception error) {
+            MessageHandler.printConsoleMessage(OutputMessageTypes.ERROR, error.getMessage());
+        }
+
+        return 0;
+    }
+
+    public static void main(String [] args) throws IOException {
+        System.out.println("\n============= WELCOME TO WEBSITE CODE CLONER =============\n");
+        try {
+            option = ChooseOptions.generateOptions();
+
+            switch (option) {
+                case 1:
+                    getUrl.getWebsiteUrl();
+                    doYouWantContinue();
+                    break;
+                case 0:
+                    exit();
+                    break;
+                default:
+                    MessageHandler.printConsoleMessage(OutputMessageTypes.ERROR, "Incorrect choice!");
+            }
+        } catch (Exception error) {
+            MessageHandler.printConsoleMessage(OutputMessageTypes.ERROR, error.getMessage());
+        }
     }
 }
