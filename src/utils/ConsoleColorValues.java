e1ebe521f7cc8ff7a746400da5bda852989692cf
abi-seth <abiheloaf@gmail.com>
2022-02-09 11:10:27 +0200

    Feat: optimized

--- src/utils/ConsoleColorValues.java
+++ src/utils/ConsoleColorValues.java
@@ -4,7 +4,7 @@ public class ConsoleColorValues {
     private static String RED = "\u001B[31m";
     private static String GREEN = "\u001B[32m";
     private static String YELLOW = "\u001B[33m";
-//    private static String BLUE = "\u001B[34;9m";
+    // private static String BLUE = "\u001B[34;9m";
     private static String BLUE = "\u001B[34m";
     private static String RESET = "\u001B[0m";
 
